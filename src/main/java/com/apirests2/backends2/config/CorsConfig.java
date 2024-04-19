e927ef73208ccbe2ab557bbd40b2b48a8b06d99b
Luisa Chavarria <luisachavarria06@gmail.com>
2024-04-19 18:07:16 -0500

    Carga 1 del proyecto backends2, con error en la conexion a la base de datos. Este error lo seguire revisando para hacer la correcion lo mas pronto posible.

--- src/main/java/com/apirests2/backends2/config/CorsConfig.java
+++ src/main/java/com/apirests2/backends2/config/CorsConfig.java
@@ -1,5 +1,21 @@
 package com.apirests2.backends2.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+        .allowedOrigins("*")//Escribir la url del from a conectar
+        .allowedMethods("*")//Podemos especificar los metodos a permitir desde el front, GET, POST, ECT.
+        .allowCredentials(true);
+    }
+
 
 }
